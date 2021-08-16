61981a1a4541364835e89b97dca928d71b974c20
Pavel Baldin <dizel_tdi@bk.ru>
2021-08-16 22:10:27 +0300

    fix: fix errors

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
 module.exports = function toReadable(number) {
-  var string = number.toString(), units, tens, start, hundred, end, chunks, chunk;
-  if (parseInt(string) === 0) {
-  return 'zero';
-  }
-  units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-  start = string;
-  if (start<20){
-      return units[string]
-  }
-  if (start>19 && start<100){ 
-    chunks=[];
-    chunks=string.split('');
-    chunk=chunks[0];
-    end=[];
-    end=chunks[1];
-  return `${tens[chunk]} ${units[end]}`
-  }
-if (start>99 && start<1000){
-    chunks=[];
-    chunks=string.split('');
-    chunk=chunks[1];
-    end=[];
-    end=chunks[2];
-hundred=[];
-hundred=chunks[0];
-  return `${units[hundred]} hundred ${tens[chunk]} ${units[end]}`
-}
-}
+  var string = number.toString(), units, tens, start, end, chunks, chunksLen, chunk, ints, i, word, words;
+    if (parseInt(string) === 0) {
+    return 'zero';
+    }
+    units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    start = string.length;
+    chunks = [];
+    while (start > 0) {
+    end = start; 
+    chunks.push(string.slice((start = Math.max(0, start - 3)), end));
+    }
+    chunksLen = chunks.length;
+    words = [];
+    for (i = 0; i < chunksLen; i++) {
+    chunk = parseInt(chunks[i]);
+    if (chunk) {
+    ints = chunks[i].split('').reverse().map(parseFloat);
+    if (ints[1] === 1) {
+    ints[0] += 10;
+    }
+    if ((word = units[ints[0]])) {
+    words.push(word);
+    }
+    if ((word = tens[ints[1]])) {
+    words.push(word);
+    }
+    if (ints[0] || ints[1]) {
+    }
+    if ((word = units[ints[2]])) {
+    words.push(word + ' hundred');
+    }
+    }
+    }
+    return words.reverse().join(' ');
+    }
+    
