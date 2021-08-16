6c1b1dba002d258d65240e3fa87d12ffee3952bd
Pavel Baldin <dizel_tdi@bk.ru>
2021-08-16 20:30:31 +0300

    init: start script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    var string = number.toString(), units, tens, start, hundred, end, chunks, chunk;
+    if (parseInt(string) === 0) {
+    return 'zero';
+    }
+    units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+   
 }
